cb4e1bfdf5a22266f24e0069edb71615e609ec6a
bort0 <bort0@SKINPC>
2022-07-25 20:13:13 -0300

    - routes created for CRUD

---
+++ server/controller/tasks.controller.js
@@ -0,0 +1,15 @@
+export const getTasks = (req, res) => {
+    res.send('getting tasks...');
+};
+export const getTask = (req, res) => {
+    res.send('getting task...');
+};
+export const createTask = (req, res) => {
+    res.send('creating task...');
+};
+export const updateTask = (req, res) => {
+    res.send('updateing task...');
+};
+export const deleteTask = (req, res) => {
+    res.send('deleting task...');
+};
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from "express";  
 import {PORT} from "./config.js";
 import ping from "./routes/index.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
+
 
 const app = express();
 
 app.use(ping);
+app.use(tasksRoutes);
+
 app.listen(PORT);
 console.log(`Server run in port: ${PORT}`);
