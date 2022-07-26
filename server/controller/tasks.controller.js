e139a2b68d4cfa79275c6e34f5fbf0ec6799964c
bort0 <bort0@SKINPC>
2022-07-25 21:02:48 -0300

    getTask, getTasks Methods

--- server/controller/tasks.controller.js
+++ server/controller/tasks.controller.js
@@ -1,9 +1,21 @@
 import { pool } from "../db.js";
 
-export const getTasks = (req, res) => {
+export const getTasks = async (req, res) => {
+    const [result] = await pool.query(
+        'SELECT * FROM tasks ORDER BY createdAt ASC'
+        );
+    res.json(result);
     res.send('getting tasks...');
 };
-export const getTask = (req, res) => {
+export const getTask = async (req, res) => {
+    const [result] = await pool.query(
+        'SELECT * FROM tasks WHERE id = ? ORDER BY createdAt ASC',
+        [req.params.id]
+        );
+    if (result.length == 0) 
+        return res.status(404).json({ message: "Task not found."});
+    
+    res.json(result[0]);
     res.send('getting task...');
 };
 
